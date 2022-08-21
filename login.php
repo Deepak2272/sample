0f469f9592be7f2b383f4f7ba8aca0eb9fc5f1f3
Deepak2272 <104371928+Deepak2272@users.noreply.github.com>
2022-08-21 13:21:22 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,61 @@
+<?php
+
+include 'config.php';
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+
+   $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $row = mysqli_fetch_assoc($select);
+      $_SESSION['user_id'] = $row['id'];
+      header('location:home.php');
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body >
+   
+<div class="form-container" style="background: #f093fb;
+background: -webkit-linear-gradient(to bottom right, rgba(240, 147, 251, 1), rgba(245, 87, 108, 1));
+background: linear-gradient(to bottom right, rgba(240, 147, 251, 1), rgba(245, 87, 108, 1))">
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <h3><img src="logo.svg" width="33px" padding-top="11px" padding-right="3px">  login now</h3>
+      <?php
+      if(isset($message)){
+         foreach($message as $message){
+            echo '<div class="message">'.$message.'</div>';
+         }
+      }
+      ?>
+      <input type="email" name="email" placeholder="enter email" class="box" required>
+      <input type="password" name="password" placeholder="enter password" class="box" required>
+      <input type="submit" name="submit" value="login now" class="btn">
+      <p>don't have an account? <a href="register.php">regiser now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
